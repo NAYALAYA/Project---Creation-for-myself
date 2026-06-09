96294aeaa5eb601b4c993b2a037111ee19d730ee
NAYALAYA <student.171773@worcesterschools.net>
2026-06-09 11:09:26 -0400

    Add files via upload

---
+++ projevy/main.py
@@ -0,0 +1,38 @@
+# Welcome & Description of Project
+
+username = input("What should I call you?")
+
+print()
+
+print("Welcome,", username,", to your Storyline Revision Organizer.")
+
+print()
+
+print("With this guide, you'll be able to write about specific facts about the book you are reading.")
+print("In this, you can also write about your own thoughts and predictions about the story. This will help you keep track of your story and better your understanding.")
+
+# The list of things you can do
+
+
+
+# Maybe a little picture
+
+# Finally, show list and ask user what they would like to do
+
+# What choosing edit leads to
+
+# Choices of what they can edit and how to end it
+
+# Option to view instead of edit
+
+# Choices of what they can view
+
+# Clear option 
+
+# Tell user what it does
+
+# Ask user if they wish to proceed
+
+# If user wished to proceed, Clear all
+
+# Repeat everything, but asking for username and welcome statement
