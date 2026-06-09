e2544a2000b52ba37c8e40e6beb186c7a689bdcc
NAYALAYA <student.171773@worcesterschools.net>
2026-06-09 13:40:58 -0400

    Added the questions and options

--- projevy/main.py
+++ projevy/main.py
@@ -8,8 +8,7 @@ print("Welcome,", username,", to your Storyline Revision Organizer.")
 
 print()
 
-print("With this guide, you'll be able to write specific facts about the book you are reading.")
-print("In this, you can also write about your own thoughts and predictions about the story. This will help you think back on your story and better your understanding.")
+print("With this guide, you'll be asked questions regarding the story you've read about. As you answer said questions")
 
 # The list of things you can do
 
@@ -21,7 +20,7 @@ print("View--This allows you to view what you have written in whichever section 
 print("Clear--This will clear everything you have written, but only in the section of your choosing.")
 print("Clear All--This clears all your writing and starts you from the beginning.")
 
-MEnU = ["edit", "view", "clear all", "clear"]
+# MEnU = ["edit", "view", "clear all", "clear"]
 
 # Maybe a little picture
 
@@ -45,14 +44,99 @@ print("What would you like to do?")
 choice = input(" ")
 
 # What choosing edit leads to
-''''''
-#if choice == "edit":
-''''''
+
+if choice == "edit":
+    print("What would you like to edit?")
+    print("Choose a number between 0 and 22")
+    choice = input(" ")
+
 # Choices of what they can edit and how to end it
 
-# Option to view instead of edit
+print("Prompts:")
 
-# Choices of what they can view
+print()
+
+print("1. Explain the title.")
+
+print()
+
+print("2. What category or genre do you think it fits into?")
+
+print()
+
+print("3. What do you think the author's purpose was?")
+
+print()
+
+print("4. Something you liked about it.")
+
+print()
+
+print("5. Something you disliked about it.")
+
+print()
+
+print("6. Describe the setting.")
+
+print()
+
+print("7. Which character did you like most?")
+
+print()
+
+print("8. Which character did you like least?")
+
+print()
+
+print("9. Describe one of the main characters.")
+
+print()
+
+print("10. What changes does a main character go through?")
+
+print()
+
+print("11. Describe one significant episode.")
+
+print()
+
+print("12. What techniques does the author use to tell the story?")
+
+print()
+
+print("13. How did reading it change you, or your views?")
+
+print()
+
+print("14. What would you say to persuade a friend to read it or not to read it?")
+
+print()
+
+print("15. Summarise it in one written sentence or a one minute speech.")
+
+print()
+
+print("16. What feedback would you give the author?")
+
+print()
+
+print("17. How might you have written it differently?")
+
+print()
+
+print("18. What do you think of the ending?")
+
+print()
+
+print("19. What happens, or should happen, after the ending?")
+
+print()
+
+print("20. What would you want to read about in a sequel or prequel?")
+
+print()
+
+print("21. Choices of what they can view")
 
 # Clear option 
 
